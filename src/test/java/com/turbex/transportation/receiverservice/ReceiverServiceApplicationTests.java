9ed0aa13eb392cb03af0f567af656765f431955c
Lucas Mezuraro <lucaspmmezuraro@gmail.com>
2020-09-03 22:26:21 -0300

    initial config

---
+++ src/test/java/com/turbex/transportation/receiverservice/ReceiverServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turbex.transportation.receiverservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceiverServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
