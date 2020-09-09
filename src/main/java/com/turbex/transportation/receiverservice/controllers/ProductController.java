2fc730086739f50296460b72bf67e884d2854780
Lucas Mezuraro <lucaspmmezuraro@gmail.com>
2020-09-09 16:23:19 -0300

    first tests in product controller layer

--- src/main/java/com/turbex/transportation/receiverservice/controllers/ProductController.java
+++ src/main/java/com/turbex/transportation/receiverservice/controllers/ProductController.java
@@ -1,4 +1,24 @@
 package com.turbex.transportation.receiverservice.controllers;
 
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/product")
 public class ProductController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Object> get() {
+        return ResponseEntity.ok("Working");
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Object> findOne(@PathVariable("id")  Long id) {
+        return ResponseEntity.ok(id);
+    }
+
 }
--- src/test/java/com/turbex/transportation/receiverservice/controllers/ProductControllerTest.java
+++ src/test/java/com/turbex/transportation/receiverservice/controllers/ProductControllerTest.java
@@ -2,19 +2,47 @@ package com.turbex.transportation.receiverservice.controllers;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Optional;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @SpringBootTest
+@AutoConfigureMockMvc
 public class ProductControllerTest {
 
     @Autowired
+    MockMvc mockMvc;
+
+    @Mock
     ProductController productController;
 
     @Test
     public void mustBeExists() {
         Assert.assertNotNull("", Optional.ofNullable(productController));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void shouldReturnSuccess() throws Exception {
+        this.mockMvc.perform(get("/product/")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print()).andExpect(status()
+                        .is2xxSuccessful());
+    }
+
+    @org.junit.jupiter.api.Test
+    public void shouldReturnSuccessulWhenSetParam() throws Exception {
+        this.mockMvc.perform(get("/product/{id}", 1)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print()).andExpect(status()
+                .is2xxSuccessful());
     }
 }
